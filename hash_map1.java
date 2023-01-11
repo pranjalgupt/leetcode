1f44c0e3ab2233a59673c295a7fc5cd60dbd7af3
pranjalgupt <guptapranjal54321@gmail.com>
2023-01-11 18:38:20 +0530

    day1

---
+++ hash_map1.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class hash_map1 {
+    public static void main(String args[]) {
+        HashMap <Integer,String> hash_map1 = new HashMap <Integer,String>();
+        HashMap <Integer,String> hash_map2 = new HashMap <Integer,String>();
+        hash_map1.put(1,"Red");
+        hash_map1.put(2,"Green");
+        hash_map1.put(3,"Black");
+        System.out.println
+
+    }
+    
+}
