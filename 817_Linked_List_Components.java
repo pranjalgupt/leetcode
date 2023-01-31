19afedeb4be8ebe9807b62d47db185065922a310
pranjalgupt <guptapranjal54321@gmail.com>
2023-01-31 18:41:07 +0530

    linklist

---
+++ 817_Linked_List_Components.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int numComponents(ListNode head, int[] nums) {
+        int max=0;
+        for(int x:nums)
+        {
+            if(x>max)
+                max=x;
+        }
+        int[] a= new int[max+1];
+        
+        for(int x:nums)
+            a[x]++;
+        
+        int c=0;
+        int components=0;
+        while(head!=null)
+        {
+            if(head.val<=max&&a[head.val]==1){
+                c++;
+            }
+            else{
+                if(c>0){
+                    components++;
+                    c=0;
+                }
+            }
+            head=head.next;
+        }
+        if(c>0)
+            components++;
+        return components;
+    }
+}
