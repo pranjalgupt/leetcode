750e587de83ddbbfb549d930a70d9a33a6b387f6
pranjalgupt <guptapranjal54321@gmail.com>
2023-03-19 22:10:50 +0530

    binary search

---
+++ 700. Search in a Binary Search Tree.java
@@ -0,0 +1,17 @@
+class Solution {
+    public TreeNode searchBST(TreeNode root, int val) {
+        while (root!=null) {
+            if (root.val==val) {
+                return root;
+            }  
+            else if (root.val<val) {
+                root = root.right;
+            }
+            else {
+                root = root.left;
+            } 
+        }
+        return null;
+    }
+}
+
